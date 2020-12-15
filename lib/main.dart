4dad091125434e01e981ab4b3e10fcefbcdb18f8
Lyle Dane M. Carcedo <lylecarcedo@gmail.com>
2020-12-15 18:52:47 +0700

    Finalizing a little bit to make the program run

--- lib/main.dart
+++ lib/main.dart
@@ -1,93 +1,93 @@
-// import 'package:flutter/material.dart';
-// import 'models/meal.dart';
-// import 'screens/meals/dummy_data.dart';
-// import 'screens/tabs/main.dart';
-// import 'screens/categories/main.dart';
-// import 'screens/meals/main.dart';
-// import 'screens/meal_detail/main.dart';
-// import 'screens/settings/main.dart';
+import 'package:delicious_meals/screens/tabs/main.dart';
+import 'package:flutter/material.dart';
+import 'models/meal.dart';
+import 'screens/meals/dummy_data.dart';
+import 'screens/categories/main.dart';
+import 'screens/meals/main.dart';
+import 'screens/meal_detail/main.dart';
+import 'screens/settings/main.dart';
 
-// void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
-// class MyApp extends StatefulWidget {
-//   @override
-//   _MyAppState createState() => _MyAppState();
-// }
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-// class _MyAppState extends State<MyApp> {
-//   Map<String, bool> _filters = {
-//     'gluten': false,
-//     'lactose': false,
-//     'vegan': false,
-//     'vegetarian': false,
-//   };
+class _MyAppState extends State<MyApp> {
+  Map<String, bool> _filters = {
+    'gluten': false,
+    'lactose': false,
+    'vegan': false,
+    'vegetarian': false,
+  };
 
-//   List<Meal> _availableMeals = DUMMY_MEALS;
-//   List<Meal> _favoriteMeals = [];
+  List<Meal> _availableMeals = DUMMY_MEALS;
+  List<Meal> _favoriteMeals = [];
 
-//   void _setFilters(Map<String, bool> filterData) {
-//     setState(() {
-//       _filters = filterData;
+  void _setFilters(Map<String, bool> filterData) {
+    setState(() {
+      _filters = filterData;
 
-//       _availableMeals = DUMMY_MEALS.where((meal) {
-//         if(_filters['gluten'] && !meal.isGlutenFree) return false;
-//         if(_filters['lactose'] && !meal.isLactoseFree) return false;
-//         if(_filters['vegan'] && meal.isVegan) return false;
-//         if(_filters['vegetarian'] && !meal.isVegetarian) return false;
+      _availableMeals = DUMMY_MEALS.where((meal) {
+        if(_filters['gluten'] && !meal.isGlutenFree) return false;
+        if(_filters['lactose'] && !meal.isLactoseFree) return false;
+        if(_filters['vegan'] && meal.isVegan) return false;
+        if(_filters['vegetarian'] && !meal.isVegetarian) return false;
 
-//         return true;
-//       }).toList();
-//     });
-//   }
+        return true;
+      }).toList();
+    });
+  }
 
-//   void _toggleFavorite(String mealId) {
-//     final existingIndex = _favoriteMeals.indexWhere((meal) => meal.id == mealId);
+  void _toggleFavorite(String mealId) {
+    final existingIndex = _favoriteMeals.indexWhere((meal) => meal.id == mealId);
 
-//     setState(() {
-//       if(existingIndex > -1)  {
-//         _favoriteMeals.removeAt(existingIndex);
-//       } else {
-//         _favoriteMeals.add(DUMMY_MEALS.firstWhere((meal) => meal.id == mealId));
-//       }
-//     });
-//   }
+    setState(() {
+      if(existingIndex > -1)  {
+        _favoriteMeals.removeAt(existingIndex);
+      } else {
+        _favoriteMeals.add(DUMMY_MEALS.firstWhere((meal) => meal.id == mealId));
+      }
+    });
+  }
 
-//   bool _isMealFavorite(String mealId) {
-//     return _favoriteMeals.any((meal) => meal.id == mealId);
-//   }
+  bool _isMealFavorite(String mealId) {
+    return _favoriteMeals.any((meal) => meal.id == mealId);
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'DeliMeals',
-//       theme: ThemeData(
-//         primarySwatch: Colors.indigo,
-//         accentColor: Colors.amber,
-//         canvasColor: Color.fromRGBO(220, 220, 200, 1),
-//         fontFamily: 'Raleway',
-//         textTheme: ThemeData.light().textTheme.copyWith(
-//               bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-//               bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-//               headline6: TextStyle(
-//                 fontSize: 18,
-//                 fontFamily: 'RobotoCondensed',
-//                 fontWeight: FontWeight.bold,
-//                 color: Colors.white,
-//               ),
-//             ),
-//       ),
-//       home: TabsScreen(_favoriteMeals),
-//       routes: {
-//         MealsScreen.route: (ctx) => MealsScreen(_availableMeals),
-//         MealDetailScreen.route: (ctx) => MealDetailScreen(_toggleFavorite, _isMealFavorite),
-//         SettingsScreen.route: (ctx) => SettingsScreen(_filters, _setFilters),
-//       },
-//       onUnknownRoute: (settings) {
-//         return MaterialPageRoute(
-//           builder: (ctx) => CategoriesScreen(),
-//         );
-//       },
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(220, 220, 200, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              bodyText1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              bodyText2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
+              headline6: TextStyle(
+                fontSize: 18,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+      ),
+      home: TabsScreen(_favoriteMeals),
+      routes: {
+        MealsScreen.route: (ctx) => MealsScreen(_availableMeals),
+        MealDetailScreen.route: (ctx) => MealDetailScreen(_toggleFavorite, _isMealFavorite),
+        SettingsScreen.route: (ctx) => SettingsScreen(_filters, _setFilters),
+      },
+      onUnknownRoute: (settings) {
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
+      },
+    );
+  }
+}
